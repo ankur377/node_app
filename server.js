2e5b3a165c4bf9bd5bc2b28965daab6ff7b95d32
AnkurSoni <80379470+ankur377@users.noreply.github.com>
2022-08-22 13:06:20 +0530

    server.js removed

--- server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-const listener = app.listen(process.env.PORT || 5000, () => {
-    console.log('Your app is listening on port http://localhost:' + listener.address().port)
-})
