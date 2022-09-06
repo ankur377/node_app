184985b297c539fea1e3581de1c014ffcddb27d8
AnkurSoni <80379470+ankur377@users.noreply.github.com>
2022-09-06 15:59:55 +0530

    some changes

--- controllers/cronjobController.js
+++ controllers/cronjobController.js
@@ -4,6 +4,6 @@ exports.cronjobController =  (req, res) => {
     cron.schedule("*/60 * * * * *", function () {
         console.log("running a task every 60 second");
     });
-    res.send("cron job Working");
+    res.send("cron job worKing");
 
 }
