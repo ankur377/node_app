4c6605595ddc8f41ea533b25e6c0d1e2cbef1f97
AnkurSoni <80379470+ankur377@users.noreply.github.com>
2022-09-07 13:43:04 +0530

    remove test file

--- controllers/childController.js
+++ controllers/childController.js
@@ -1,8 +1,6 @@
 const cp = require("child_process");
-const file = require('../helper/test');
 
 exports.childController = (req, res) => {
     cp.exec('start firefox');
-    cp.execSync('node file');
     res.send('Task Done');
 }
--- helper/test.js
+++
@@ -1,1 +0,0 @@
-console.log("this is simple texting file ");
